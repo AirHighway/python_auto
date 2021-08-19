6af4521f8c70d3a7617aaf413c9abce73b6673ec
zihuan320HeAo <86967582+zihuan320HeAo@users.noreply.github.com>
2021-08-19 10:22:50 +0800

    Create _01_multi_docxs_to_one.py
        """
        使用需记得更改源文件的目录和 <目标的完整的目录>
        美中不足的是添加的顺序是乱序的 除非文件名本来就有一定的规则（比如开头是数字或者字母）
        :param source_files_path:源文件路径
        :param result_file:目标文件的完整路径
        :return:没有返回值，直接生成一个文件后就结束进程
        """

---
+++ _01_multi_docxs_to_one.py
@@ -0,0 +1,36 @@
+# pip install pywin32
+
+import os
+import win32com.client as win32
+
+
+def merge_docx(source_files_path=r"D:\homework\all_docxs",
+               result_file=r"D:\homework\new_folder\result.docx"):
+    """
+    :param source_files_path: all docxs file path
+    :param result_file: target docxs file path
+    :return:
+    """
+    word = win32.gencache.EnsureDispatch("Word.Application")
+    word.Visible = False
+    word.DisplayAlerts = False
+    output = word.Documents.Add()
+    source_docx_path = os.listdir(source_files_path)
+    source_docxs = []
+    for file in source_docx_path:
+        if file.endswith(".docx") or file.endswith(".doc"):
+            source_docxs.append(source_files_path + "\\" + file)
+    i = 0
+    source_docxs.reverse()
+    for source_docx in source_docxs:
+        source_docx_filename_list = (source_docx.split("\\"))[-1].split(".")
+        print(source_docx_filename_list[-2])
+        if i >= 1:
+            output.Application.Selection.Range.InsertBreak()
+        output.Application.Selection.Range.InsertFile(source_docx)
+        i += 1
+    output.SaveAs(result_file)
+    output.Close()
+
+if __name__ == '__main__':
+  merge_docx()
